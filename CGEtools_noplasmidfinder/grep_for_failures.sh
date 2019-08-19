af5d8bff24ac0acdc0df553feecd0dfcc43aad07
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-08-19 11:30:00 +0100

    grep for failures in cge tools

---
+++ CGEtools_noplasmidfinder/grep_for_failures.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for i in `ls -d Sepi*/`
+  do
+    cd  $i/Assembler ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+    cd  $i/cgMLSTFinder ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+    cd  $i/ContigAnalyzer ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+    cd  $i/KmerFinder ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+    cd  $i/MLST ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+    cd  $i/ResFinder ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
+  done
+
+exit
