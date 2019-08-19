f1d31bddd9ef367856fec8a7b861a70cc143c6a9
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-08-19 11:37:03 +0100

    better failure catch for cge

--- CGEtools_noplasmidfinder/grep_for_failures.sh
+++ CGEtools_noplasmidfinder/grep_for_failures.sh
@@ -10,4 +10,6 @@ for i in `ls -d Sepi*/`
     cd  $i/ResFinder ; cat *err | grep "FAIL\|fail\|Fail\|ERROR\|error\|Error" >> failure_catch.log ; cd ../..
   done
 
+echo "script ran successfully" >> failure_catch.log
+
 exit
