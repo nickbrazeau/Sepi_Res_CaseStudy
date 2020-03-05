2d9b1cc0ba13c63a7cc2975124cf41a545fb7c14
nickbrazeau <nbrazeau@med.unc.edu>
2020-03-05 00:34:05 -0500

    assemblytics update for params

--- assemblytics/assemblytics_snk.py
+++ assemblytics/assemblytics_snk.py
@@ -2,8 +2,9 @@
 ###############################################################################
 # Purpose: Running assembyltics for Sepi
 # Author: Nick Brazeau
-#Given: fasta, vcf, etc
-#Return: bed, vcf
+#Given: ref fasta, velvet alginment
+#Return: assemblytics output
+#Notes: this is a very basic script that is using wildcards instead of a "master" head file
 ###############################################################################
 
 ####### Working Directory and Project Specifics ############
@@ -14,20 +15,27 @@ SAMPLES, = glob_wildcards(readdir + '{sample}.bam')
 ########   REFERENCE & Assemb. Paths   ##############
 REF = '/proj/ideel/resources/genomes/Sepidermidis/genomes/CORE_GCF_000007645.1_ASM764v1_genomic.fasta'
 GFF = '/proj/ideel/resources/genomes/Sepidermidis/info/gff/GCF_000007645.1_ASM764v1_genomic.gff'
-{ASSEMB} = '/nas/longleaf/home/nfb/bin/Assemblytics/scripts/ Assemblytics'
+ASSEMB = '/nas/longleaf/home/nfb/bin/Assemblytics/scripts/Assemblytics'
 
 rule all:
-	input: expand('nucmer/{sample}_nucmer.log', sample = SAMPLES)
+	input: expand('logs/{sample}_assembly.log', sample = SAMPLES)
 
 
 # Run Assemblytics https://github.com/MariaNattestad/Assemblytics
-rule run_
+# anchor is default at 10kbp
+# 2499279 is the size of the whole genome
+# note the input for the Assemblytics program is a command line parsing
+rule run_assemlytics:
+	input: 'logs/{sample}_nucmer_assembly.log'
+	params: '{sample}'
+	output: 'logs/{sample}_assembly.log'
+	shell: '{ASSEMB} {params}.delta {params} 10000 50 2499279 2> {output}'
 
 rule run_nucmer:
     input: '/proj/ideel/meshnick/users/NickB/Projects/Sepi_Res_CaseStudy/CGEtools_noplasmidfinder/{sample}_5_22_2018/Assembler/output/assembly/contigs.fa'
-    param: '{sample}'
-    output: 'nucmer/{sample}_nucmer.log'
+    params: '{sample}'
+    output: 'logs/{sample}_nucmer_assembly.log'
     shell: 'nucmer --maxmatch -minmatch 100 -mincluster 500 \
     {REF} \
     {input} \
-    --prefix={param}  2> {output} '
+    --prefix={params}  2> {output}'
---
+++ touch_files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/snpEff_genes.txt
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/snpEff_summary.html
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/kma_wrap/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/nucmer/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/variants/*
+
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/SumSTATsandQC/AlignSummary/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/SumSTATsandQC/CallableLoci/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/SumSTATsandQC/coverage/data/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/SumSTATsandQC/FlagStats/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/SumSTATsandQC/ValidateBAM/*
+touch /pine/scr/n/f/nfb/Projects/Sepi_Res_CaseStudy/wgs_pe_improved/aln/merged/*
